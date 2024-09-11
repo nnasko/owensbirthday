2b46cdd5ec2db910ea020e4c9e91b2cf3fb10979
nnasko <nnasskooo@gmail.com>
2024-09-11 15:51:05 +0100

    yolo

--- app/components/CountdownComponent.tsx
+++ app/components/CountdownComponent.tsx
@@ -1,32 +1,35 @@
-"use client"
+"use client";
 // components/Countdown.js
-import React, { useState, useEffect } from 'react';
-import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
-import '@leenguyen/react-flip-clock-countdown/dist/index.css';
-import { TypeAnimation } from 'react-type-animation';
+import React, { useState, useEffect } from "react";
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+import { TypeAnimation } from "react-type-animation";
 
 const Countdown = () => {
   return (
     <div className="text-center py-10">
       <div className="flex justify-center items-center mb-5">
-        <img className='h-40 border-4 border-black rounded-xl' src='owen.jpg' alt="Owen's Birthday"></img>
+        <img
+          className="h-40 border-4 border-black rounded-xl"
+          src="owen.jpg"
+          alt="Owen's Birthday"
+        ></img>
       </div>
       <TypeAnimation
         sequence={[
-          'Whos? Birthday is in',
+          "Whos? 50th Birthday is in",
           1000,
-          'OWENS! 🥳🥳 Birthday is in',
+          "OWENS! 🥳🥳 50th Birthday is in",
           1000,
         ]}
         speed={20}
-        className='text-white font-extrabold'
-        style={{ fontSize: '2em' }}
+        className="text-white font-extrabold"
+        style={{ fontSize: "2em" }}
         repeat={Infinity}
       />
       <div className="flex justify-center items-center">
-        <FlipClockCountdown to={"2025-04-25"} showSeparators={false}
-        > 
-        HAPPY BIRTHDAY!!! 🥳🥳
+        <FlipClockCountdown to={"2025-04-25"} showSeparators={false}>
+          HAPPY BIRTHDAY!!! 🥳🥳
         </FlipClockCountdown>
       </div>
     </div>
